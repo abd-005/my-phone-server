d2ab51d0701aa90d3509c7f706efae6f847b824c
abd-005 <abd.me7@gmail.com>
2025-10-27 22:21:31 +0600

    Json loaded in dynamic route

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const phones = require('./phones.json');
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("My phone information is coming soon!");
 });
+
+app.get('/phones', (req,res)=>{
+    res.send(phones)
+})
+
+app.get('/phone/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    console.log(`I need data for: ${id}`)
+    const phone = phones.find(phone=>phone.id===id)||{};
+    res.send(phone);
+    
+})
 
 app.listen(port, () => {
   console.log(`This app is listening to prot: ${port}`);
